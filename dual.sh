28a10f1da36d835d472bb624987246b7a305934b
Django <matheuscumth@gmail.com>
2022-10-18 20:04:18 -0300

    fix error code on find

--- dual.sh
+++ dual.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 [[ -d $1 ]] || exit 1
 
-find $1 -maxdepth $4 -type -f -exec md5sum '{}' \; | sort | uniq -Dw 32 
+find $1 -maxdepth $2 -type f -exec md5sum '{}' \; | sort | uniq -Dw 32 
